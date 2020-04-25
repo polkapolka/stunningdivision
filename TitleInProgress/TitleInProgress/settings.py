58224b17365f52679cbd286bfa67650ee4f654b4
ppolk-nocimed <ppolk@nocimed.com>
2020-04-25 11:53:53 -0700

    Basic first app

--- TitleInProgress/TitleInProgress/settings.py
+++ TitleInProgress/TitleInProgress/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'TitleInProgress.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hackathon',
+        'USER': 'hackathonuser',
+        'PASSWORD': 'hackthevirus',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
