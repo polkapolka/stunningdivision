e78c03ed1a99526556b880ea1f6cd4c9cb1c9136
ppolk-nocimed <ppolk@nocimed.com>
2020-04-25 22:52:30 -0700

    Swapped TitleInProgress for ReachCare

--- TitleInProgress/ReachCare/asgi.py
+++ TitleInProgress/ReachCare/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TitleInProgress project.
+ASGI config for ReachCare project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TitleInProgress.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ReachCare.settings')
 
 application = get_asgi_application()
--- TitleInProgress/ReachCare/urls.py
+++ TitleInProgress/ReachCare/urls.py
@@ -1,4 +1,4 @@
-"""TitleInProgress URL Configuration
+"""ReachCare URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
